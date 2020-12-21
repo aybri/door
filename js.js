1da1e7810065a3db7ad44edb2b5272a0fb58cc46
Impulse <impulsivecoder7@gmail.com>
2020-12-20 22:35:42 -0600

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function door() {
     var doorImage = document.getElementById("doorImage");
     var size = doorImage.height;
     var loop1;
-    for (loop1 = w; loop1 < 4001; loop1++) {
+    for (loop1 = size; loop1 < 4001; loop1++) {
       doorImage.height = size+loop1;
       doorImage.width = size+loop1;
     }
