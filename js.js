92f23eb15d56bd2ac3181bb1a934714bae00d79d
Impulse <impulsivecoder7@gmail.com>
2020-12-20 22:31:25 -0600

    Create js.js

---
+++ js.js
@@ -0,0 +1,10 @@
+function door() {
+    var doorImage = document.getElementById("doorImage");
+    var size = doorImage.height;
+    var loop1;
+    for (loop1 = w; loop1 < 4001; loop1++) {
+      doorImage.height = size+loop1;
+      doorImage.width = size+loop1;
+    }
+    window.location('emeraldimpulse7.github.io');
+}
